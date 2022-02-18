239e8a4e7465a588400ce32b406aecfb4e7e8776
Jennifer Ong <jong@mfour.com>
2022-02-17 16:47:53 -0800

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+//think of namespace as a folder, simple way to load classes,
+//when a controller extends this Controller composer will know they come from
+//the same namespace
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
