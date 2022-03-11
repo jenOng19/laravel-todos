0a666212e17bf75c7867f94240401af8a50533d3
Jennifer Ong <jong@mfour.com>
2022-03-11 14:55:10 -0800

    Moved common functionality between blade files into layout/app.blade.php

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,31 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap/CSS only -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <title>Todo Item</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">
-            {{ $todo->name }}
-        </h1>
+@extends('layouts.app')
 
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header">
-                        Details
-                    </div>
-                    <div class="card-body">
-                        {{ $todo->description }}
-                    </div>
-                </div>
+@section('title')
+    Single Todo: {{ $todo->name }}
+@endsection
+
+@section('content')
+
+<h1 class="text-center my-5">
+    {{ $todo->name }}
+</h1>
+
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card card-default">
+            <div class="card-header">
+                Details
+            </div>
+            <div class="card-body">
+                {{ $todo->description }}
             </div>
         </div>
     </div>
-</body>
-</html>
+</div>
+
+@endsection
